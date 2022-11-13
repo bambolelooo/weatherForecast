699151d93fa68aa428fb675ba6d5a7da5ddccadd
bambolelooo <63548697+bambolelooo@users.noreply.github.com>
2022-11-12 23:26:59 -0500

    added function do edit div

--- js/script.js
+++ js/script.js
@@ -7,14 +7,30 @@ $(function () {
     const cityDataMap =
         new Map(JSON.parse(localStorage.getItem("cityDataMap"))) || new Map();
     const cities = [...cityDataMap.keys()];
-
     $("#cities").autocomplete({
         source: cities,
     });
 
     $(".cityBtn").click(function () {
         const city = $(this).text().trim();
-        if (cityDataMap.get(city)) {
+        fetchCity(city);
+    });
+
+    function fetchCity(city) {
+        const currDate = new Date();
+        // if difference between current time and last stored data is less than 3h - dont fetch
+        if (
+            cityDataMap.get(city) &&
+            (cityDataMap.get(city).current.dt * 1000 - currDate.getTime()) /
+                3600000 <
+                3
+        ) {
+            //TODO: Fill divs
+            console.log(
+                (cityDataMap.get(city).current.dt * 1000 - currDate.getTime()) /
+                    3600000
+            );
+        } else if (cityDataMap.get(city)) {
             console.log(cityDataMap.get(city));
             fetch(
                 `https://api.openweathermap.org/data/2.5/forecast?lat=${
@@ -24,8 +40,7 @@ $(function () {
                 .then((response) => response.json())
                 .then((data) => {
                     console.log(data);
-                    //fill divs
-                    parseFutureWeather(data);
+                    parseWeather(data, city);
                 });
         } else {
             fetch(
@@ -33,37 +48,84 @@ $(function () {
             )
                 .then((response) => response.json())
                 .then((data) => {
-                    cityDataMap.set(data[0].name, {
+                    let cityName = data[0].name;
+                    cityDataMap.set(cityName, {
                         lat: data[0].lat,
                         lon: data[0].lon,
                     });
-                    console.log(cityDataMap.get(data[0].name));
+                    console.log(cityDataMap.get(cityName));
                     localStorage.setItem(
                         "cityDataMap",
                         JSON.stringify(Array.from(cityDataMap.entries()))
                     );
                     fetch(
                         `https://api.openweathermap.org/data/2.5/forecast?lat=${
-                            cityDataMap.get(data[0].name).lat
+                            cityDataMap.get(cityName).lat
                         }&lon=${
-                            cityDataMap.get(data[0].name).lon
+                            cityDataMap.get(cityName).lon
                         }&appid=${apiKey}&units=metric`
                     )
                         .then((response) => response.json())
                         .then((data) => {
                             console.log(data);
-                            parseFutureWeather(data);
+                            console.log(cityName);
+                            parseWeather(data, cityName);
                         });
                 });
         }
-    });
-});
-
-function parseFutureWeather(data) {
-    const bestTime = String(13 - data.city.timezone / 3600);
-    for (const weather of data.list) {
-        if (weather.dt_txt.slice(11, 13) === bestTime) {
-            console.log(weather);
-        }
     }
-}
+
+    function parseWeather(data, cityName) {
+        const bestTime = String(13 - data.city.timezone / 3600); // best time to present weather
+        const weatherStats = {};
+        let counter = 1;
+        for (const weather of data.list) {
+            weatherStats.current = data.list[0];
+            if (data.list[0].dt_txt.slice(11, 13) === bestTime) {
+                // skip if current time is best
+            } else if (weather.dt_txt.slice(11, 13) === bestTime) {
+                weatherStats[`day${counter}`] = weather;
+                counter++;
+            }
+        }
+
+        // xD
+        const weatherStatsClone = JSON.parse(JSON.stringify(weatherStats));
+        counter = 1;
+        cityDataMap.set(cityName, {
+            ...cityDataMap.get(cityName),
+            ...weatherStatsClone,
+        });
+        localStorage.setItem(
+            "cityDataMap",
+            JSON.stringify(Array.from(cityDataMap.entries()))
+        );
+        console.log(cityDataMap.get(cityName));
+    }
+
+    function fillDivs(city) {
+        $(".city").text(city);
+        const mainWeather = cityDataMap.get(city).current.weather[0].main;
+        const currentIcon = cityDataMap.get(city).current.weather[0].icon;
+        $(".today").html(`
+        <div class="weather rounded-4 night d-flex justify-content-between align-items-center px-3 overflow-hidden">
+        Today
+        <img src="./img/${
+            mainWeather === "Rain"
+                ? "rainy"
+                : currentIcon === "01d"
+                ? "day"
+                : currentIcon === "01n"
+                ? "night"
+                : mainWeather === "Clouds"
+                ? "cloudy"
+                : mainWeather === "Snow"
+                ? "snowy"
+                : mainWeather === "Thunderstorm"
+                ? "thunder"
+                : "cloudy"
+        }.svg" alt="rainy" />
+        </div>
+        `);
+    }
+});
