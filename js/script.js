187e086c366129a486dedd4f571df8a7a1231464
bambolelooo <63548697+bambolelooo@users.noreply.github.com>
2022-11-13 19:17:04 -0500

    almost mvp

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+"use strict";
 $(function () {
     // let weatherPads = querySelectorAll(".weather");
 
@@ -7,8 +8,29 @@ $(function () {
     const cityDataMap =
         new Map(JSON.parse(localStorage.getItem("cityDataMap"))) || new Map();
     const cities = [...cityDataMap.keys()];
+
+    function fillNavbar() {
+        for (const city of cities) {
+            $("#sidebar-nav").append(`<button
+                                    class="list-group-item border-end-0 d-inline-block cityBtn"
+                                    data-bs-parent="#sidebar"
+                                >
+                                ${city}
+                                </button>`);
+        }
+    }
+    fillNavbar();
+
     $("#cities").autocomplete({
         source: cities,
+    });
+    if (cities.length != 0) {
+        fillDivs(cities[0]);
+    }
+
+    $(".search").click(function () {
+        const city = $("#cities").val();
+        fetchCity(city);
     });
 
     $(".cityBtn").click(function () {
@@ -18,18 +40,19 @@ $(function () {
 
     function fetchCity(city) {
         const currDate = new Date();
-        // if difference between current time and last stored data is less than 3h - dont fetch
+        // if difference between current time and last stored data is less than 2h - dont fetch
         if (
             cityDataMap.get(city) &&
-            (cityDataMap.get(city).current.dt * 1000 - currDate.getTime()) /
-                3600000 <
-                3
+            Math.abs(
+                (cityDataMap.get(city).current.dt * 1000 - currDate.getTime()) /
+                    3600000
+            ) < 2
         ) {
-            //TODO: Fill divs
             console.log(
                 (cityDataMap.get(city).current.dt * 1000 - currDate.getTime()) /
                     3600000
             );
+            fillDivs(city);
         } else if (cityDataMap.get(city)) {
             console.log(cityDataMap.get(city));
             fetch(
@@ -41,6 +64,7 @@ $(function () {
                 .then((data) => {
                     console.log(data);
                     parseWeather(data, city);
+                    fillDivs(city);
                 });
         } else {
             fetch(
@@ -48,48 +72,68 @@ $(function () {
             )
                 .then((response) => response.json())
                 .then((data) => {
-                    let cityName = data[0].name;
-                    cityDataMap.set(cityName, {
-                        lat: data[0].lat,
-                        lon: data[0].lon,
-                    });
-                    console.log(cityDataMap.get(cityName));
-                    localStorage.setItem(
-                        "cityDataMap",
-                        JSON.stringify(Array.from(cityDataMap.entries()))
-                    );
-                    fetch(
-                        `https://api.openweathermap.org/data/2.5/forecast?lat=${
-                            cityDataMap.get(cityName).lat
-                        }&lon=${
-                            cityDataMap.get(cityName).lon
-                        }&appid=${apiKey}&units=metric`
-                    )
-                        .then((response) => response.json())
-                        .then((data) => {
-                            console.log(data);
-                            console.log(cityName);
-                            parseWeather(data, cityName);
+                    console.log(data);
+                    if (data.length === 0) {
+                        $("#cities").val("Couldn't find");
+                    } else {
+                        let cityName = data[0].name;
+                        cityDataMap.set(cityName, {
+                            lat: data[0].lat,
+                            lon: data[0].lon,
                         });
+                        console.log(cityDataMap.get(cityName));
+                        localStorage.setItem(
+                            "cityDataMap",
+                            JSON.stringify(Array.from(cityDataMap.entries()))
+                        );
+                        fetch(
+                            `https://api.openweathermap.org/data/2.5/forecast?lat=${
+                                cityDataMap.get(cityName).lat
+                            }&lon=${
+                                cityDataMap.get(cityName).lon
+                            }&appid=${apiKey}&units=metric`
+                        )
+                            .then((response) => response.json())
+                            .then((data) => {
+                                console.log(data);
+                                console.log(cityName);
+                                parseWeather(data, cityName);
+                                fillDivs(cityName);
+                                if (
+                                    ![...cityDataMap.keys()].includes(cityName)
+                                ) {
+                                    fillNavbar();
+                                }
+                            });
+                    }
                 });
         }
     }
 
     function parseWeather(data, cityName) {
-        const bestTime = String(13 - data.city.timezone / 3600); // best time to present weather
+        console.log(`parsing data for ${cityName}`);
+        let bestTime = 13 - data.city.timezone / 3600; // best time to present weather
+        while (bestTime % 3 !== 0) {
+            bestTime += 1;
+        }
+        bestTime = String(bestTime);
         const weatherStats = {};
         let counter = 1;
+        weatherStats.localHours = new Date(
+            new Date().getTime() + data.city.timezone * 1000
+        ).getUTCHours();
+        weatherStats.current = data.list[0];
         for (const weather of data.list) {
-            weatherStats.current = data.list[0];
             if (data.list[0].dt_txt.slice(11, 13) === bestTime) {
                 // skip if current time is best
             } else if (weather.dt_txt.slice(11, 13) === bestTime) {
+                console.log("found best time");
                 weatherStats[`day${counter}`] = weather;
                 counter++;
             }
         }
 
-        // xD
+        // genius way to make a deep copy of an object xD
         const weatherStatsClone = JSON.parse(JSON.stringify(weatherStats));
         counter = 1;
         cityDataMap.set(cityName, {
@@ -105,27 +149,70 @@ $(function () {
 
     function fillDivs(city) {
         $(".city").text(city);
-        const mainWeather = cityDataMap.get(city).current.weather[0].main;
-        const currentIcon = cityDataMap.get(city).current.weather[0].icon;
+        const data = cityDataMap.get(city);
+        const currentMainWeather = data.current.weather[0].main;
+        const currentIcon = data.current.weather[0].icon;
+        const currentHours = data.localHours;
+        const dayNight =
+            currentHours > 18 || currentHours < 6 ? "night" : "day";
+        const currentTemp = Math.round(data.current.main.temp);
         $(".today").html(`
-        <div class="weather rounded-4 night d-flex justify-content-between align-items-center px-3 overflow-hidden">
-        Today
+        <div class="weather rounded-4 ${
+            dayNight === "day" && currentTemp > 10
+                ? "day"
+                : dayNight === "day" && currentTemp < 10
+                ? "cold"
+                : "night"
+        } d-flex justify-content-between align-items-center px-3 overflow-hidden text-center py-2">
+        Today<br>
+        Temperature: ${currentTemp}°C<br>
+        Wind speed: ${data.current.wind.speed} m/s<br>
+        Humidity: ${data.current.main.humidity}%
         <img src="./img/${
-            mainWeather === "Rain"
+            currentMainWeather === "Rain"
                 ? "rainy"
                 : currentIcon === "01d"
                 ? "day"
                 : currentIcon === "01n"
                 ? "night"
-                : mainWeather === "Clouds"
+                : currentMainWeather === "Clouds"
                 ? "cloudy"
-                : mainWeather === "Snow"
+                : currentMainWeather === "Snow"
                 ? "snowy"
-                : mainWeather === "Thunderstorm"
+                : currentMainWeather === "Thunderstorm"
                 ? "thunder"
                 : "cloudy"
-        }.svg" alt="rainy" />
+        }.svg"/>
         </div>
         `);
+        for (let i = 1; i <= 5; i++) {
+            const dayMainWeather = data[`day${i}`].weather[0].main;
+            const dayIcon = data[`day${i}`].weather[0].icon;
+            const dayTemp = Math.round(data[`day${i}`].main.temp);
+            $(`.day${i}`).html(`
+            <div
+            class="weather rounded-4 ${
+                dayTemp > 10 ? "day" : "cold"
+            } d-flex justify-content-between align-items-center p-3 my-3 overflow-hidden">
+            ${data[`day${i}`].dt_txt.slice(0, 11)} <br>
+            Temperature: ${dayTemp}°C<br>
+        Wind speed: ${data[`day${i}`].wind.speed} m/s<br>
+        Humidity: ${data[`day${i}`].main.humidity}%
+        <img src="./img/${
+            dayMainWeather === "Rain"
+                ? "rainy"
+                : dayIcon === "01d"
+                ? "day"
+                : dayMainWeather === "Clouds"
+                ? "cloudy"
+                : dayMainWeather === "Snow"
+                ? "snowy"
+                : dayMainWeather === "Thunderstorm"
+                ? "thunder"
+                : "cloudy"
+        }.svg"/>
+            </div>
+            `);
+        }
     }
 });
